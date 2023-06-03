6a5badc181d8f555ecc82e40905973f62be151ac
alfathlathif <alfathns1932@mail.ugm.ac.id>
2023-06-04 00:01:14 +0700

    add handler for login and register page

--- my-app/src/components/RegisterPage/register.jsx
+++ my-app/src/components/RegisterPage/register.jsx
@@ -1,11 +1,56 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    retype_password: "",
+    textChange: "Register",
+  });
+
+  const navigate = useNavigate();
+  const { username, password, retype_password, textChange } = formData;
+  const handleInput = (text) => (e) => {
+    setFormData({ ...formData, [text]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username && password) {
+      if (password === retype_password) {
+        setFormData({ ...formData, textChange: "Submitting" });
+        axios
+          .post("http://localhost:8081/auth/register/", formData)
+          .then((res) => {
+            if (res.status >= 200 && res.status < 300) {
+              toast.success(res.data);
+              setTimeout(() => {
+                navigate("/login")
+              }, 1000);
+            } else {
+              toast.error(res.data);
+            }
+          })
+          .catch((err) => console.log(err));
+      } else {
+        toast.error("Password tidak sesuai");
+      }
+    } else {
+      toast.error("Isikan seluruh informasi yang dibutuhkan");
+    }
+  };
+
   return (
     <div className="bg-bg_regis_login bg-cover bg-no-repeat bg-bottom min-h-[calc(100vh-160px)] overflow-x-hidden">
       <div className="mt-[50px] flex justify-center items-center">
-        <form className="bg-white shadow-md rounded w-1/2 px-8 pt-6 pb-8 mb-4">
+        <ToastContainer />
+        <form
+          className="bg-white shadow-md rounded w-1/2 px-8 pt-6 pb-8 mb-4"
+          onSubmit={handleSubmit}
+        >
           <div className="font-bold text-[#2F311B] text-[24px] mb-6 flex justify-center">
             Create Your Account
           </div>
@@ -14,7 +59,8 @@ const Register = () => {
               <div className="text-[16px] text-[#808194]">Username</div>
               <input
                 className="shadow border rounded-lg w-full py-3.5 px-3 text-[16px] text-gray-700 leading-tight focus:outline-none mb-4"
-                id="name"
+                name="username"
+                onChange={handleInput("username")}
                 type="text"
                 placeholder="Enter your username"
               />
@@ -23,7 +69,8 @@ const Register = () => {
               <div className="text-[16px] text-[#808194]">Password</div>
               <input
                 className="shadow border rounded-lg w-full py-3.5 px-3 text-[16px] text-gray-700 leading-tight focus:outline-none mb-4"
-                id="name"
+                name="password"
+                onChange={handleInput("password")}
                 type="password"
                 placeholder="Enter your password"
               />
@@ -32,7 +79,8 @@ const Register = () => {
               <div className="text-[16px] text-[#808194]">Confirm Password</div>
               <input
                 className="shadow border rounded-lg w-full py-3.5 px-3 text-[16px] text-gray-700 leading-tight focus:outline-none"
-                id="name"
+                name="retype_password"
+                onChange={handleInput("retype_password")}
                 type="password"
                 placeholder="Confirm your password"
               />
@@ -45,7 +93,9 @@ const Register = () => {
           </div>
           <div className="flex item-center mt-4">
             <p className="mr-[4px] text-[16px]">Already have an accout?</p>
-            <Link to="/login" className="text-[16px] text-[#868358]">Login</Link>
+            <Link to="/login" className="text-[16px] text-[#868358]">
+              Login
+            </Link>
           </div>
         </form>
       </div>
